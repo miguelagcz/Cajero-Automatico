b254cf2f8540bb44272439eb08c2d8c2fb7d2e96
Miguel Angel <miguel12agcz@gmail.com>
2022-10-03 19:56:24 -0500

    nuevas cantidades agregadas asi como la funcionalidad del mismo cajero

--- cajero.js
+++ cajero.js
@@ -1,51 +1,61 @@
 class Billete
 {
- constructor (v, c)
- {
+   constructor(v, c)
+   {
     this.valor = v;
     this.cantidad = c;
- }
-
+   }
 }
-
-
-function entregardinero()
+function entregarDinero()
 {
-    for(var bi of caja) 
-    {
-        if(dinero > 0)
-        {
-           div = Math.floor(dinero / bi.valor);
-
-            if(div > bi.cantidad)
-            {
-              papeles = bi.cantidad;
-            }
-            else
-            {
-                papeles = div;    
-            }
-            entregado.push( new Billete(bi.valor, papeles) );
-            dinero = dinero - (bi.valor * papeles);
-        }
-    }
-
+  var t = document.getElementById ("dinero");
+  dinero = parseInt(t.value);
+  for(var bi of caja)
+  {
     if(dinero > 0)
     {
-        console.log("soy un cajero pobre y no tengo dinero :c")
+      div = Math.floor(dinero / bi.valor);
+
+      if  (div > bi.cantidad)
+      {
+        papeles = bi.cantidad;
+      }
+
+      else{
+        papeles = div;
+      }
+
+      entregado.push( new  Billete(bi.valor, papeles) );
+      dinero -=  (bi.valor * papeles ); 
     }
-      console.log(entregado);
+
+  }
+  if(dinero > 0)
+  {
+    resultado.innerHTML = "soy un cajero malo, he sido muy malo   y no puedo darte esa cantidad :(";  
+  }
+  else
+  {
+    for (var e of entregado)
+    {
+      if(e.cantidad > 0)
+      {
+        resultado.innerHTML +=  e.cantidad + " billetes de $" + e.valor + "<br />";
+
+      }
+    }
+  }
 }
-
-
 var caja = [];
-var  entregado = [];
+var entregado = [];
+caja.push( new Billete(125, 5) );
 caja.push( new Billete(50, 20) );
-caja.push( new Billete(20, 30) );
+caja.push( new Billete(20, 5) );
 caja.push( new Billete(10, 10) );
-var dinero = 1000;
+caja.push( new Billete(5, 5) );
+var dinero = 0;
 var div = 0;
 var papeles = 0;
-
+var resultado = document.getElementById("resultado");
 var b = document.getElementById("extraer");
-document.addEventListener("click", entregardinero);
+document.addEventListener("click",entregarDinero);
